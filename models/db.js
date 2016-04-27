fedd3b6417783682a2de60956e4ab7f222785ae8
zhoujialin <724475200@qq.com>
2016-04-27 15:47:20 +0800

    db更新

--- models/db.js
+++ models/db.js
@@ -2,8 +2,8 @@
  * Created by zhoujialin on 2016/3/18.
  */
 var mongoose = require('mongoose');
-var uri = 'mongodb://siteAdmin:meigui12160@127.0.0.1:27017/node_club';
-//var uri = 'mongodb://127.0.0.1/node_club';
+//var uri = 'mongodb://siteAdmin:meigui12160@127.0.0.1:27017/node_club';
+var uri = 'mongodb://127.0.0.1/node_club';
 mongoose.connect(uri);
 
 exports.mongoose = mongoose;
